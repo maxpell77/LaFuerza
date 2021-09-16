9471b95ce84503551dc561f94ff4edee21ea0a81
Maximiliano Pell <maxpell@192.168.1.44>
2021-09-15 21:12:11 -0300

    final

--- src/laFuerza/GeneradorResumenSistema.java
+++ src/laFuerza/GeneradorResumenSistema.java
@@ -1,6 +1,5 @@
 package laFuerza;
 
-
 import java.io.IOException;
 
 import java.util.ArrayList;
@@ -18,6 +17,7 @@ public abstract class GeneradorResumenSistema {
 	private static int CUPOS_TOTALES = 0;
 	private static int TOTAL_CUPOS_DISPONIBLES = 0;
 	private static int MAXIMO_VENTAS_NO_CONCRETADAS = 0;
+	private static Map<String, Integer> RANKING_TIPO_ATRACCION;
 
 	public static void generarResuemSistema(LinkedList<Propuesta> propuestas, int cantidadUsuariosCompradores)
 			throws IOException {
@@ -34,11 +34,17 @@ public abstract class GeneradorResumenSistema {
 
 		Map<Integer, ArrayList<Propuesta>> propuestasContratadas = new TreeMap<Integer, ArrayList<Propuesta>>(
 				Collections.reverseOrder());
+		RANKING_TIPO_ATRACCION = new TreeMap<String, Integer>(Collections.reverseOrder());
+
 		ArrayList<Propuesta> aux;
+		Integer key;
+
+		Integer aux2;
+		String key2;
 
 		for (Propuesta propuesta : propuestas) {
 
-			Integer key = propuesta.getCosto() * propuesta.getCantidadComprada();
+			key = propuesta.getCosto() * propuesta.getCantidadComprada();
 			if (propuestasContratadas.containsKey(key)) {
 				aux = propuestasContratadas.get(key);
 			} else {
@@ -46,7 +52,18 @@ public abstract class GeneradorResumenSistema {
 			}
 			aux.add(propuesta);
 			propuestasContratadas.put(key, aux);
+
+			key2 = propuesta.getTipoAtraccion().getNombre();
+			if (RANKING_TIPO_ATRACCION.containsKey(key2)) {
+				aux2 = RANKING_TIPO_ATRACCION.get(key2);
+			} else {
+				aux2 = 0;
+			}
+			aux2 += propuesta.getCantidadComprada();
+			RANKING_TIPO_ATRACCION.put(key2, aux2);
+
 		}
+
 		return propuestasContratadas;
 
 	}
@@ -68,51 +85,70 @@ public abstract class GeneradorResumenSistema {
 				if (propuesta.getClass() == Atraccion.class) {
 					calculosOtrosDatos(propuesta);
 				}
-				
 
 				mensaje += propuesta.getNombre() + "," + propuesta.getTipoAtraccion().getNombre() + ","
 						+ cadaPropuesta.getKey() + "," + propuesta.getCantidadComprada() + ","
 						+ propuesta.getCantidadRechazada() + "\n";
-
 			}
-
 		}
 		mensaje += "TOTALES" + ",," + TOTAL_INGRESOS + "," + TOTAL_COMPRAS + "," + TOTAL_RECHAZOS + "\n";
 
 		return mensaje;
 	}
-	
-	
+
 	private static void calculosOtrosDatos(Propuesta propuesta) {
 		Atraccion atraccion = (Atraccion) propuesta;
 		CUPOS_TOTALES += atraccion.getCupoInicial();
 		TOTAL_CUPOS_DISPONIBLES += atraccion.getCupoDisponible();
 		MAXIMO_VENTAS_NO_CONCRETADAS += atraccion.getCupoDisponible() * atraccion.getCosto();
-		
+
 	}
-	
-	
 
 	public static String generarArchivoGeneral(int cantidadUsuariosCompradores) {
-
-		float capacidadOciosa = (float) ((TOTAL_CUPOS_DISPONIBLES * 100) / CUPOS_TOTALES);
-		int ticketPromedio = TOTAL_INGRESOS / cantidadUsuariosCompradores;
-
 		String mensaje = "";
 
 		mensaje += "Resumen general.\n\n";
-		mensaje += "Se han realizado ventas a " + cantidadUsuariosCompradores + " usuarios, por un total de "
-				+ TOTAL_INGRESOS + " créditos galácticos (ticket promedio de " + ticketPromedio
-				+ " créditos galácticos por usuario).\n\n";
 
-		mensaje += "En total se contrataron " + TOTAL_COMPRAS + " propuestas y se han rechazado " + TOTAL_RECHAZOS
-				+ ".\n\n";
-		mensaje += "De un máximo de " + CUPOS_TOTALES + " cupos disponibles, quedaron " + TOTAL_CUPOS_DISPONIBLES
-				+ " por vender. Lo que representa una capacidad ociosa del " + capacidadOciosa
-				+ "%, equivalente a un máximo de ventas no concretadas de " + MAXIMO_VENTAS_NO_CONCRETADAS
-				+ " créditos galácticos.\n\n";
+		if (cantidadUsuariosCompradores == 0) {
+			mensaje += "No se han realizado ninguna venta.\n\n";
+			mensaje += "Se ofrecieron " + TOTAL_RECHAZOS + " propuestas a "
+					+ CargadorArchivosEntrada.getUsuarios().size() + " usuarios.\n\n";
+
+		} else {
+			float capacidadOciosa = (float) ((TOTAL_CUPOS_DISPONIBLES * 100) / CUPOS_TOTALES);
+
+			int ticketPromedio = TOTAL_INGRESOS / cantidadUsuariosCompradores;
+
+			mensaje += "Se han realizado ventas a " + cantidadUsuariosCompradores + " usuarios, por un total de "
+					+ TOTAL_INGRESOS + " créditos galácticos (ticket promedio de " + ticketPromedio
+					+ " créditos galácticos por usuario).\n\n";
+			mensaje += "En total se contrataron " + TOTAL_COMPRAS + " propuestas y se han rechazado " + TOTAL_RECHAZOS
+					+ ".\n\n";
+			mensaje += "De un máximo de " + CUPOS_TOTALES + " cupos disponibles, quedaron " + TOTAL_CUPOS_DISPONIBLES
+					+ " por vender. Lo que representa una capacidad ociosa del " + capacidadOciosa
+					+ "%, equivalente a un máximo de ventas no concretadas de " + MAXIMO_VENTAS_NO_CONCRETADAS
+					+ " créditos galácticos.\n\n";
+			mensaje += ladoGanador() + "\n\n";
+
+		}
 
 		mensaje += "¡Que la Fuerza te acompañe!";
+
+		return mensaje;
+
+	}
+
+	private static String ladoGanador() {
+		String mensaje = "";
+		int ladoOscuro = RANKING_TIPO_ATRACCION.get("Lado Oscuro");
+		int ladoLuminoso = RANKING_TIPO_ATRACCION.get("Lado Luminoso");
+
+		if (ladoOscuro > ladoLuminoso) {
+			mensaje = "La Fuerza está del Lado Oscuro.";
+		} else if (ladoLuminoso > ladoOscuro) {
+			mensaje = "La Fuerza está del Lado Luminoso.";
+		} else
+			mensaje = "La Fuerza esta equilibrada.";
 
 		return mensaje;
 

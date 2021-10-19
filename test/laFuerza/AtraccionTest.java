72f72d226cce449aa567bb7942f5fc2e6195eaf3
maxpell77 <77016821+maxpell77@users.noreply.github.com>
2021-10-19 19:08:30 -0300

    revisar y rearmar test

--- test/laFuerza/AtraccionTest.java
+++ test/laFuerza/AtraccionTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 
 public class AtraccionTest {
 
-	@Test
-	public void actualizarCupoDisponibleTest() {
-		Atraccion atC100T10 = new Atraccion(100, 10, TipoAtraccion.LADO_OSCURO, 5, "at1");
-		assertTrue(atC100T10.hayCupoDisponible());
-		atC100T10.actualizarCupoDisponible();
-		assertEquals(4, atC100T10.getCupoDisponible());
-	}
+//	@Test
+//	public void actualizarCupoDisponibleTest() {
+//		Atraccion atC100T10 = new Atraccion(100, 10, TipoAtraccion.LADO_OSCURO, 5, "at1");
+//		assertTrue(atC100T10.hayCupoDisponible());
+//		atC100T10.actualizarCupoDisponible();
+//		assertEquals(4, atC100T10.getCupoDisponible());
+//	}
 
 }
--- test/laFuerza/GeneradorIntinerarioUsuarioTest.java
+++ test/laFuerza/GeneradorIntinerarioUsuarioTest.java
@@ -20,43 +20,43 @@ public class GeneradorIntinerarioUsuarioTest {
 	LinkedList<Atraccion> atraccionesIncluidas = new LinkedList<Atraccion>();
 	Promocion prC150T15;
 
-	@Before
-	public void setUp() {
-		// C=costo , T=tiempo
-		atC100T10 = new Atraccion(100, 10, TipoAtraccion.LADO_OSCURO, 100, "at1");
-		atC150T5 = new Atraccion(150, 5, TipoAtraccion.LADO_OSCURO, 100, "at2");
-		usC300T30 = new Usuario("u1", TipoAtraccion.LADO_OSCURO, 300, 30);
-		usC100T30 = new Usuario("u2", TipoAtraccion.LADO_OSCURO, 100, 30);
-		usC200T20 = new Usuario("u3", TipoAtraccion.LADO_OSCURO, 200, 20);
-		atraccionesIncluidas.add(atC100T10);
-		atraccionesIncluidas.add(atC150T5);
-		prC150T15 = new PromoAbsoluta(TipoAtraccion.LADO_OSCURO, "promo1", "descripcion", atraccionesIncluidas, 150);
-		prC150T15.setCosto();
-		prC150T15.setTiempoUtilizado();
-	}
+//	@Before
+//	public void setUp() {
+//		// C=costo , T=tiempo
+//		atC100T10 = new Atraccion(100, 10, TipoAtraccion.LADO_OSCURO, 100, "at1");
+//		atC150T5 = new Atraccion(150, 5, TipoAtraccion.LADO_OSCURO, 100, "at2");
+//		usC300T30 = new Usuario("u1", TipoAtraccion.LADO_OSCURO, 300, 30);
+//		usC100T30 = new Usuario("u2", TipoAtraccion.LADO_OSCURO, 100, 30);
+//		usC200T20 = new Usuario("u3", TipoAtraccion.LADO_OSCURO, 200, 20);
+//		atraccionesIncluidas.add(atC100T10);
+//		atraccionesIncluidas.add(atC150T5);
+//		prC150T15 = new PromoAbsoluta(TipoAtraccion.LADO_OSCURO, "promo1", "descripcion", atraccionesIncluidas, 150);
+//		prC150T15.setCosto();
+//		prC150T15.setTiempoUtilizado();
+//	}
 
-	@Test
-	public void resumenIntinerarioNoCompraTest() throws IOException {
-		String mensaje = GeneradorIntinerarioUsuario.generarIntinerario(usC300T30);
-		EscritorArchivosSalida.escribirArchivos("test/salida/u1_Real.txt", mensaje);
-		assertEquals(-1, compararArchivosLineaporLinea("test/salida/u1_Esperado.txt", "test/salida/u1_Real.txt"));
-	}
-
-	@Test
-	public void resumenIntinerarioCompraAtraccionTest() throws IOException {
-		usC100T30.agregarPropuestaAceptada(atC100T10);
-		String mensaje = GeneradorIntinerarioUsuario.generarIntinerario(usC100T30);
-		EscritorArchivosSalida.escribirArchivos("test/salida/u2_Real.txt", mensaje);
-		assertEquals(-1, compararArchivosLineaporLinea("test/salida/u2_Esperado.txt", "test/salida/u2_Real.txt"));
-	}
-
-	@Test
-	public void resumenIntinerarioCompraPromocionTest() throws IOException {
-		usC200T20.agregarPropuestaAceptada(prC150T15);
-		String mensaje = GeneradorIntinerarioUsuario.generarIntinerario(usC200T20);
-		EscritorArchivosSalida.escribirArchivos("test/salida/u3_Real.txt", mensaje);
-		assertEquals(-1, compararArchivosLineaporLinea("test/salida/u3_Esperado.txt", "test/salida/u3_Real.txt"));
-	}
+//	@Test
+//	public void resumenIntinerarioNoCompraTest() throws IOException {
+//		String mensaje = GeneradorIntinerarioUsuario.generarIntinerario(usC300T30);
+//		EscritorArchivosSalida.escribirArchivos("test/salida/u1_Real.txt", mensaje);
+//		assertEquals(-1, compararArchivosLineaporLinea("test/salida/u1_Esperado.txt", "test/salida/u1_Real.txt"));
+//	}
+//
+//	@Test
+//	public void resumenIntinerarioCompraAtraccionTest() throws IOException {
+//		usC100T30.agregarPropuestaAceptada(atC100T10);
+//		String mensaje = GeneradorIntinerarioUsuario.generarIntinerario(usC100T30);
+//		EscritorArchivosSalida.escribirArchivos("test/salida/u2_Real.txt", mensaje);
+//		assertEquals(-1, compararArchivosLineaporLinea("test/salida/u2_Esperado.txt", "test/salida/u2_Real.txt"));
+//	}
+//
+//	@Test
+//	public void resumenIntinerarioCompraPromocionTest() throws IOException {
+//		usC200T20.agregarPropuestaAceptada(prC150T15);
+//		String mensaje = GeneradorIntinerarioUsuario.generarIntinerario(usC200T20);
+//		EscritorArchivosSalida.escribirArchivos("test/salida/u3_Real.txt", mensaje);
+//		assertEquals(-1, compararArchivosLineaporLinea("test/salida/u3_Esperado.txt", "test/salida/u3_Real.txt"));
+//	}
 
 	private long compararArchivosLineaporLinea(String string, String string2) throws IOException {
 		FileReader fl1 = null;
